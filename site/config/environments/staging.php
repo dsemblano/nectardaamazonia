a401763cedfa5f9f9e8160cf8121a6a61185e899
Daniel Semblano <dsemblano@gmail.com>
2025-04-28 01:54:45 -0300

    staging debug

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -17,3 +17,10 @@ use Roots\WPConfig\Config;
 
 Config::define('WP_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
+Config::define('SAVEQUERIES', true);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', false);
+Config::define('SCRIPT_DEBUG', true);
+
+ini_set('display_errors', '1');
