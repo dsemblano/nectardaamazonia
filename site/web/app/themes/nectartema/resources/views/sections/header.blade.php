d33519b07e3c8c9d0fff67ee4cd71fe3451b754e
Daniel Semblano <dsemblano@gmail.com>
2025-04-29 05:05:15 -0300

    Improving menu

--- site/web/app/themes/nectartema/resources/views/sections/header.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner border-gray-100 border-b-2 w-full z-50 bg-white fixed top-0 left-0 shadow">
   <div class="container">
-    <div class="flex flex-row justify-between items-center gap-20 lg:justify-start lg:flex-wrap lg:text-2xl text-p">
+    <div class="flex flex-row justify-between items-center lg:justify-start lg:flex-wrap lg:text-2xl text-p">
           @include('partials.logo')
         {{-- <a class="brand" href="{{ home_url('/') }}">
           <span class="block">{!! $siteName !!}</span>
