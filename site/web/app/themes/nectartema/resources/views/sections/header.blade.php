5b20417e10ad5c230f679cd24f7c3c30e7712095
Daniel Semblano <dsemblano@gmail.com>
2025-04-29 17:20:35 -0300

    Improving pages header and H1, testing SVG css honeycomb background

--- site/web/app/themes/nectartema/resources/views/sections/header.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/header.blade.php
@@ -1,21 +1,18 @@
 <header class="banner border-gray-100 border-b-2 w-full z-50 bg-white fixed top-0 left-0 shadow">
-  <div class="container">
     <div class="flex flex-row justify-between items-center lg:justify-start lg:flex-wrap lg:text-2xl text-p">
-          @include('partials.logo')
+        @include('partials.logo')
         {{-- <a class="brand" href="{{ home_url('/') }}">
           <span class="block">{!! $siteName !!}</span>
           <span>{!! $descName !!}</span>
         </a> --}}
 
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {{-- {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!} --}}
-      <div class="flex flex-wrap lg:flex-nowrap justify-between items-center mx-auto">   
-        @include('partials.menu')
-      </div>
-    </nav>
-  @endif
+        @if (has_nav_menu('primary_navigation'))
+            <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+                {{-- {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!} --}}
+                <div class="flex flex-wrap lg:flex-nowrap justify-between items-center mx-auto">
+                    @include('partials.menu')
+                </div>
+            </nav>
+        @endif
     </div>
-  </div>
-  
 </header>
