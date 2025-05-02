eec39100ba391e07ce7c5d2a2d2d716636d8b89a
Daniel Semblano <dsemblano@gmail.com>
2025-05-02 07:10:59 -0300

    CSS adjustments for headings on home and pages

--- site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
+++ site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
       @include('sections.header')
 
-      <main id="main" class="main mt-24 lg:pt-6 flex-1">
+      <main id="main" class="main mt-14 pt-6 flex-1">
         {{-- Só para as págins Woocommerce --}}
         @if (is_woocommerce() || is_cart() || is_checkout() || is_account_page())
         <div class="container woocommerce-wrapper">
@@ -27,7 +27,7 @@
         </div>
             
         {{-- Quando for páginas menos a home --}}
-        @elseif (! is_front_page() && ! is_home())
+        @elseif (! is_front_page() && ! is_home() )
         {{-- <div class="page-wrapper bg-green-400"> --}}
           @yield('content')
         {{-- </div> --}}
--- site/web/app/themes/nectartema/resources/views/template-home.blade.php
+++ site/web/app/themes/nectartema/resources/views/template-home.blade.php
@@ -6,7 +6,9 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
+  @if (! is_front_page() && ! is_home() )
     @include('partials.page-header')
+  @endif
     {{-- @include('partials/home.hero') --}}
     @include('partials.content-page')
   @endwhile
