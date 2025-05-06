f8782a9d0c174ee43a23ec45a84905872758e08c
Daniel Semblano <dsemblano@gmail.com>
2025-05-06 12:23:35 -0300

    Page improvements

--- site/web/app/themes/nectartema/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/content-page.blade.php
@@ -1,14 +1,6 @@
-@if (! is_front_page() && ! is_home() )
+{{-- @if (! is_front_page() && ! is_home() ) --}}
 
-  <div class="container mx-auto">
-      <div class="section-page prose prose-xl max-w-full">
-        @php(the_content())
-      </div>
-  </div>
-
-@else
-  @php(the_content())
-@endif
+@php(the_content())
 
 @if ($pagination())
   <nav class="page-nav" aria-label="Page">
