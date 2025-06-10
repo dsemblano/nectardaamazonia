b33f9266dfa0defa81f83f722942ad4a61892e52
Daniel Semblano <dsemblano@gmail.com>
2025-06-10 20:11:23 -0300

    Hamburger icon adjustment for mobile

--- site/web/app/themes/nectartema/resources/views/partials/menu.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/menu.blade.php
@@ -13,7 +13,7 @@
     </ul>
 
     <!-- Mobile Toggle Button (Hidden on desktop) -->
-    <button @click.stop="mobileOpen = !mobileOpen" class="lg:hidden p-2 relative w-10 h-10 z-50"
+    <button @click.stop="mobileOpen = !mobileOpen" class="lg:hidden p-2 relative w-10 h-10 z-50 left-12 top-4"
         :aria-expanded="mobileOpen">
         <svg x-show="!mobileOpen" class="absolute inset-0 w-full h-full" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
