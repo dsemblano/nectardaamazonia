54e3fff81c7d8f7feba4c1a813a436c4c14cdd4b
Daniel Semblano <dsemblano@gmail.com>
2025-06-13 17:53:09 -0300

    Logo with new quality, adjustments menu

--- site/web/app/themes/nectartema/resources/views/partials/logo.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/logo.blade.php
@@ -1,5 +1,5 @@
 <a class="brand relative" href="{{ home_url('/') }}">
-    <img id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/Logo-nectar-da-amazonia.webp') }}">
+    <img id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/logonectarnovo.webp') }}">
 {{-- <div id="logo" class="flex flex-row items-center">
     @include('partials/icons.nectar', ['width' => '60px', 'height' => '50px'])
     <div id="nectar_logo" class="flex flex-col relative top-1">
