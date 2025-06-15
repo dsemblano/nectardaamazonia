ca6860caf678316df7a51346a64bb192da6a6a06
Daniel Semblano <dsemblano@gmail.com>
2025-06-15 17:07:21 -0300

    footer span tag paired

--- site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
@@ -1,9 +1,10 @@
 <footer class="content-info bg-fundo">
-    <div class="container prose lg:prose-h2:text-lg max-w-none prose-a:no-underline prose-a:hover:underline prose-ul:list-none">
+    <div
+        class="container prose lg:prose-h2:text-lg max-w-none prose-a:no-underline prose-a:hover:underline prose-ul:list-none">
         @php(dynamic_sidebar('sidebar-footer'))
         <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2">
-            <span class="z-10 font-bold">© 2018 - {{ date('Y') }} Néctar da Amazônia<span
-                    class="sup align-text-bottom">&reg;</span>
+            <span class="z-10 font-bold">© 2018 - {{ date('Y') }} Néctar da Amazônia<span id="trademark" class="sup align-text-bottom">&reg;</span>
+            </span>
         </div>
     </div>
 
