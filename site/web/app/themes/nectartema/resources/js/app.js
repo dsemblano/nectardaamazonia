e66c15c9795d20efb54e0389f86ae906355213f6
Daniel Semblano <dsemblano@gmail.com>
2025-06-22 00:27:59 -0300

    logo adjustments without classes

--- site/web/app/themes/nectartema/resources/js/app.js
+++ site/web/app/themes/nectartema/resources/js/app.js
@@ -6,31 +6,42 @@ window.Alpine = Alpine
 Alpine.start()
 
 // Wait for the page to load (including LCP)
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.animate_scroll');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const sections = document.querySelectorAll('.animate_scroll');
 
-  // Configure Intersection Observer
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // Step 1: Make element visible (remove opacity:0)
-        entry.target.classList.add('animated');
+//   // Configure Intersection Observer
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         // Step 1: Make element visible (remove opacity:0)
+//         entry.target.classList.add('animated');
         
-        // Step 2: Apply Animate.css effect after a tiny delay
-        setTimeout(() => {
-          entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
-        }, 10); // Short delay ensures CSS transition applies
+//         // Step 2: Apply Animate.css effect after a tiny delay
+//         setTimeout(() => {
+//           entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
+//         }, 10); // Short delay ensures CSS transition applies
         
-        observer.unobserve(entry.target);
-      }
-    });
-  }, { threshold: 0.1 });
+//         observer.unobserve(entry.target);
+//       }
+//     });
+//   }, { threshold: 0.1 });
 
-  // Observe all target sections
-  sections.forEach((section) => {
-    observer.observe(section);
-  });
-});
+//   // Observe all target sections
+//   sections.forEach((section) => {
+//     observer.observe(section);
+//   });
+// });
+
+// cd top
+// document.addEventListener("DOMContentLoaded", function() {
+//     //hide or show the "back to top" link
+
+//     //smooth scroll to top
+//     document.querySelector('.cd-top').addEventListener('click', function(event) {
+//         event.preventDefault();
+//         window.scrollTo({top: 0, behavior: 'smooth'});
+//     });
+// });
 
 // animate
 const element = document.querySelector('h1');
