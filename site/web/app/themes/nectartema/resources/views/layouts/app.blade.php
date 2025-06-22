327f0c4b7aec0b9e103e961455b1b4dcebf95d42
Daniel Semblano <dsemblano@gmail.com>
2025-06-22 18:06:16 -0300

    preload lcp home image

--- site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
+++ site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     @php(do_action('get_header'))
     @php(wp_head())
     <link rel="preload" fetchpriority="high" as="image" href="{{ Vite::asset('resources/images/logonectarnovo.webp') }}" type="image/webp">
+    @if (is_front_page() || is_home() )
+    <link rel="preload" fetchpriority="high" as="image" href="src="src="/app/uploads/2024/11/PNG-Mel-home-nectar-da-amazonia.webp" type="image/webp">
+    @endif
     <link rel="preload" href="{{ Vite::asset('resources/fonts/Poppins/Poppins-Regular.ttf') }}" as="font" type="font/ttf" crossorigin>
     <link rel="author" type="text/plain" href="{{ Vite::asset('resources/fonts/humans.txt') }}" />
     @include('partials.gtag')
