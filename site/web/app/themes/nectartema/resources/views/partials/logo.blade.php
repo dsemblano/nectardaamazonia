8af4e7a2aae5a55b4b7d3416116359a3dfe16cfe
Daniel Semblano <dsemblano@gmail.com>
2025-06-22 00:56:50 -0300

    logo adjustments testing 2

--- site/web/app/themes/nectartema/resources/views/partials/logo.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/logo.blade.php
@@ -1,4 +1,4 @@
-<a class="brand w-[302px] lg:w-auto" href="{{ home_url('/') }}">
+<a class="brand block max-w-[302px] w-full" href="{{ home_url('/') }}">
   <div class="aspect-[302/84] w-full">
     <img width="302" height="84" class="w-full h-full object-contain block" id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/logonectarnovo.webp') }}" />
   </div>
