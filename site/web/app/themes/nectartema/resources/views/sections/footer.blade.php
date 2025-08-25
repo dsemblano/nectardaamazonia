9acdf27d84954b31dca79f3f0b179c49a28b70ae
Daniel Semblano <dsemblano@gmail.com>
2025-08-25 18:17:15 -0300

    Fix style for all pages footer

--- site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="content-info bg-fundo">
     <div
-        class="container prose lg:prose-h2:text-lg max-w-none prose-a:no-underline prose-a:hover:underline prose-ul:list-none">
+        class="container max-w-none">
         @php(dynamic_sidebar('sidebar-footer'))
         <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2">
             <span class="z-10 font-bold">© 2018 - {{ date('Y') }} Néctar da Amazônia<span id="trademark" class="sup align-text-bottom">&reg;</span>
