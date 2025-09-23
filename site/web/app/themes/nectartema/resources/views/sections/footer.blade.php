21c090b1a09d41c6b9868b291513d0ca9436940a
Daniel Semblano <dsemblano@gmail.com>
2025-09-23 02:23:00 -0300

    Home link on footer

--- site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
@@ -3,7 +3,9 @@
         class="container max-w-none">
         @php(dynamic_sidebar('sidebar-footer'))
         <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2">
-            <span class="z-10 font-bold">© 2018 - {{ date('Y') }} Néctar da Amazônia<span id="trademark" class="sup align-text-bottom">&reg;</span>
+            <span class="z-10 font-bold">© 2018 - {{ date('Y') }} 
+                <a class="hover:underline" href="{{ home_url('/') }}">Néctar da Amazônia</a>
+                <span id="trademark" class="sup align-text-bottom">&reg;</span>
             </span>
         </div>
     </div>
