6f91aec48cf24dd552a0a22bf278e2001ad7bd79
Daniel Semblano <dsemblano@gmail.com>
2025-09-30 10:20:08 -0300

    height: 100vh for hero section

--- site/web/app/themes/nectartema/resources/views/sections/header.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/header.blade.php
@@ -1,4 +1,4 @@
-<header class="banner w-full z-50 bg-fundo top-0 left-0">
+<header class="banner w-full z-50 top-0 left-0 bg-fundo shadow-md">
     <div class="flex flex-row lg:flex-col lg:text-2xl text-p container py-4 gap-4">
         @include('partials.logo')
         @if (has_nav_menu('primary_navigation'))
