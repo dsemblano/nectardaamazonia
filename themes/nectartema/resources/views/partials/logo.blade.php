ccff40f9947f42023e5fcd12a5c8577386aae70e
Daniel Semblano <dsemblano@gmail.com>
2025-11-30 03:31:50 -0300

    commit revert: all changes to staging

--- themes/nectartema/resources/views/partials/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<a class="brand block max-w-[302px] w-full" href="{{ home_url('/') }}">
-  <div class="aspect-[302/84] w-full">
-    <img width="302" height="84" class="w-full h-full object-contain block hover:scale-105 transition duration-300 ease-in-out" id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/logonectarnovo.webp') }}" />
-  </div>
-</a>
