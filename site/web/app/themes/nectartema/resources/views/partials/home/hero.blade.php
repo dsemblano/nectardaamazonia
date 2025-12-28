23750e3c5d16be9323cd66d390d7ac1b4a7724c7
Daniel Semblano <dsemblano@gmail.com>
2025-12-28 19:33:53 -0300

    Staging: hero redesign shadown

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -8,3 +8,22 @@
             mel agora!</a>
     </div>
 </section>
+
+<section id="quem-somos"
+    class="bg-primary prose lg:prose-2xl max-w-none prose-a:no-underline prose-h2:text-white prose-p:text-white">
+    <div class="container flex flex-col lg:flex-row items-center gap-8 lg:gap-16 py-12 lg:py-24">
+        <div class="hero-text grow-0 shrink-1 w-full lg:w-3/5">
+            <h2>Quem somos</h2>
+            <p>
+                Na Néctar da Amazônia, nosso objetivo vai além da produção de mel. Sob a liderança de Richardson Frazão,
+                biólogo e CEO com 25 anos de experiência, desenvolvemos soluções sustentáveis, unindo meliponicultura e
+                preservação ambiental, com foco nos ODS e diretrizes ESG.
+            </p>
+            <a class="px-4 py-4 rounded-lg text-xl font-semibold transition bg-melescuro hover:bg-primary text-white" href="/quem-somos">Mais sobre nós</a>
+        </div>
+        <picture class="grow-0 shrink-1 w-full lg:w-2/5">
+            <img src="{{ Vite::asset('resources/images/home-Melipona-fulva-TIW-Expedicao-nov2023.webp') }}"
+                alt="Melkiponário" class="rounded-xl">
+        </picture>
+    </div>
+</section>
