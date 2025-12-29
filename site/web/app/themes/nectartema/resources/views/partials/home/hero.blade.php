84004ca6ea749623c76c4a8ae3ef2046c1e1a559
Daniel Semblano <dsemblano@gmail.com>
2025-12-29 10:55:41 -0300

    Staging: Quem somos section

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -1,8 +1,9 @@
 <section id="hero" class="text-white container h-fit">
     <div class="py-16 text-left prose  prose-a:no-underline">
         <h1 class="text-white text-4xl md:text-6xl">Mel Sustentável da Amazônia</h1>
-        <h2 class="text-white font-normal text-sm md:text-xl mb-10">Néctar da Amazônia: mel 100% puro de abelhas sem no-underline
-            forma artesanal no Amapá e alinhado à bioeconomia e ao desenvolvimento sustentável.</h2>
+        <h2 class="text-white font-normal text-sm md:text-xl mb-10">Néctar da Amazônia: mel 100% puro de abelhas sem
+            ferrão,
+            artesanal, no Amapá e alinhado à bioeconomia e ao desenvolvimento sustentável.</h2>
         <a href="/loja"
             class="px-4 py-4 rounded-lg text-xl font-semibold transition bg-melescuro hover:bg-primary text-white">Compre
             mel agora!</a>
@@ -10,16 +11,17 @@
 </section>
 
 <section id="quem-somos"
-    class="bg-primary prose lg:prose-2xl max-w-none prose-a:no-underline prose-h2:text-white prose-p:text-white">
+    class="bg-white">
     <div class="container flex flex-col lg:flex-row items-center gap-8 lg:gap-16 py-12 lg:py-24">
-        <div class="hero-text grow-0 shrink-1 w-full lg:w-3/5">
-            <h2>Quem somos</h2>
-            <p>
+        <div class="hero-text grow-0 shrink-1 w-full lg:w-3/5 prose lg:prose-2xl max-w-none prose-a:no-underline ">
+            <h2 class="text-verde">Quem somos</h2>
+            <p  class="text-verde">
                 Na Néctar da Amazônia, nosso objetivo vai além da produção de mel. Sob a liderança de Richardson Frazão,
                 biólogo e CEO com 25 anos de experiência, desenvolvemos soluções sustentáveis, unindo meliponicultura e
                 preservação ambiental, com foco nos ODS e diretrizes ESG.
             </p>
-            <a class="px-4 py-4 rounded-lg text-xl font-semibold transition bg-melescuro hover:bg-primary text-white" href="/quem-somos">Mais sobre nós</a>
+            <a class="px-4 py-4 rounded-lg text-xl font-semibold transition bg-melescuro hover:bg-primary text-white"
+                href="/quem-somos">Mais sobre nós</a>
         </div>
         <picture class="grow-0 shrink-1 w-full lg:w-2/5">
             <img src="{{ Vite::asset('resources/images/home-Melipona-fulva-TIW-Expedicao-nov2023.webp') }}"
