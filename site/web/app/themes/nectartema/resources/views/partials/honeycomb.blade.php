4d53e87b67869173961483267f31be8f432a5f98
Daniel Semblano <dsemblano@gmail.com>
2026-01-04 20:56:34 -0300

    testing honeycomb with grids

---
+++ site/web/app/themes/nectartema/resources/views/partials/honeycomb.blade.php
@@ -0,0 +1,177 @@
+<div id="o-time" class="max-w-5xl mx-auto px-4 py-12">
+  <h3 class="text-2xl md:text-3xl font-light text-center mb-10">
+    O time
+  </h3>
+
+  <!-- GRID -->
+  <div
+    class="
+      grid
+      grid-cols-2
+      md:grid-cols-3
+      gap-x-4
+      md:gap-x-6
+      gap-y-12
+      justify-items-center
+    "
+  >
+    <!-- HEX 1 -->
+    <div class="w-40 md:w-48 relative">
+      <!-- SVG 1 -->
+      <!-- (unchanged SVG code) -->
+      <svg width="256" viewBox="0 0 31 29" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="hexClip1">
+            <path d="M9.00035 7.55536L2.02871 11.4285C1.39378 11.7812 1 12.4505 1 13.1768V21.8232C1 22.5495 1.39378 23.2188 2.02871 23.5715L9.00035 27.4446C9.61973 27.7887 10.3749 27.7794 10.9857 27.4202L17.514 23.58C18.1249 23.2206 18.5 22.5648 18.5 21.8561V13.1439C18.5 12.4352 18.1249 11.7794 17.514 11.42L10.9857 7.57981C10.375 7.22056 9.61973 7.21126 9.00035 7.55536Z"/>
+          </clipPath>
+        </defs>
+
+        <g transform="translate(6 -3)">
+          <image
+            href="{{ Vite::asset('resources/images/bee.webp') }}"
+            x="-4"
+            y="-2"
+            width="40"
+            height="40"
+            preserveAspectRatio="xMidYMid slice"
+            clip-path="url(#hexClip1)"
+          />
+        </g>
+      </svg>
+
+      <div class="absolute inset-x-0 bottom-0 text-xs uppercase text-center">
+        Richardson Frazão
+      </div>
+    </div>
+
+    <!-- HEX 2 (desktop stagger) -->
+    <div class="w-40 md:w-48 relative md:translate-y-12">
+            <svg width="256" viewBox="0 0 31 29" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="hexClip1">
+            <path d="M9.00035 7.55536L2.02871 11.4285C1.39378 11.7812 1 12.4505 1 13.1768V21.8232C1 22.5495 1.39378 23.2188 2.02871 23.5715L9.00035 27.4446C9.61973 27.7887 10.3749 27.7794 10.9857 27.4202L17.514 23.58C18.1249 23.2206 18.5 22.5648 18.5 21.8561V13.1439C18.5 12.4352 18.1249 11.7794 17.514 11.42L10.9857 7.57981C10.375 7.22056 9.61973 7.21126 9.00035 7.55536Z"/>
+          </clipPath>
+        </defs>
+
+        <g transform="translate(6 -3)">
+          <image
+            href="{{ Vite::asset('resources/images/bee.webp') }}"
+            x="-4"
+            y="-2"
+            width="40"
+            height="40"
+            preserveAspectRatio="xMidYMid slice"
+            clip-path="url(#hexClip1)"
+          />
+        </g>
+      </svg>
+      <div class="absolute inset-x-0 bottom-0 text-xs uppercase text-center">
+        Ana Laura Frazão
+      </div>
+    </div>
+
+    <!-- HEX 3 -->
+    <div class="w-40 md:w-48 relative">
+              <svg width="256" viewBox="0 0 31 29" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="hexClip1">
+            <path d="M9.00035 7.55536L2.02871 11.4285C1.39378 11.7812 1 12.4505 1 13.1768V21.8232C1 22.5495 1.39378 23.2188 2.02871 23.5715L9.00035 27.4446C9.61973 27.7887 10.3749 27.7794 10.9857 27.4202L17.514 23.58C18.1249 23.2206 18.5 22.5648 18.5 21.8561V13.1439C18.5 12.4352 18.1249 11.7794 17.514 11.42L10.9857 7.57981C10.375 7.22056 9.61973 7.21126 9.00035 7.55536Z"/>
+          </clipPath>
+        </defs>
+
+        <g transform="translate(6 -3)">
+          <image
+            href="{{ Vite::asset('resources/images/bee.webp') }}"
+            x="-4"
+            y="-2"
+            width="40"
+            height="40"
+            preserveAspectRatio="xMidYMid slice"
+            clip-path="url(#hexClip1)"
+          />
+        </g>
+      </svg>
+      <div class="absolute inset-x-0 bottom-0 text-xs uppercase text-center">
+        Francisco Junior
+      </div>
+    </div>
+
+    <!-- HEX 4 -->
+    <div class="w-40 md:w-48 relative">
+              <svg width="256" viewBox="0 0 31 29" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="hexClip1">
+            <path d="M9.00035 7.55536L2.02871 11.4285C1.39378 11.7812 1 12.4505 1 13.1768V21.8232C1 22.5495 1.39378 23.2188 2.02871 23.5715L9.00035 27.4446C9.61973 27.7887 10.3749 27.7794 10.9857 27.4202L17.514 23.58C18.1249 23.2206 18.5 22.5648 18.5 21.8561V13.1439C18.5 12.4352 18.1249 11.7794 17.514 11.42L10.9857 7.57981C10.375 7.22056 9.61973 7.21126 9.00035 7.55536Z"/>
+          </clipPath>
+        </defs>
+
+        <g transform="translate(6 -3)">
+          <image
+            href="{{ Vite::asset('resources/images/bee.webp') }}"
+            x="-4"
+            y="-2"
+            width="40"
+            height="40"
+            preserveAspectRatio="xMidYMid slice"
+            clip-path="url(#hexClip1)"
+          />
+        </g>
+      </svg>
+      <div class="absolute inset-x-0 bottom-0 text-xs uppercase text-center">
+        Júlio Avelar
+      </div>
+    </div>
+
+    <!-- HEX 5 (desktop stagger) -->
+    <div class="w-40 md:w-48 relative md:translate-y-12">
+              <svg width="256" viewBox="0 0 31 29" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="hexClip1">
+            <path d="M9.00035 7.55536L2.02871 11.4285C1.39378 11.7812 1 12.4505 1 13.1768V21.8232C1 22.5495 1.39378 23.2188 2.02871 23.5715L9.00035 27.4446C9.61973 27.7887 10.3749 27.7794 10.9857 27.4202L17.514 23.58C18.1249 23.2206 18.5 22.5648 18.5 21.8561V13.1439C18.5 12.4352 18.1249 11.7794 17.514 11.42L10.9857 7.57981C10.375 7.22056 9.61973 7.21126 9.00035 7.55536Z"/>
+          </clipPath>
+        </defs>
+
+        <g transform="translate(6 -3)">
+          <image
+            href="{{ Vite::asset('resources/images/bee.webp') }}"
+            x="-4"
+            y="-2"
+            width="40"
+            height="40"
+            preserveAspectRatio="xMidYMid slice"
+            clip-path="url(#hexClip1)"
+          />
+        </g>
+      </svg>
+      <div class="absolute inset-x-0 bottom-0 text-xs uppercase text-center">
+        Arnaldo Francisco
+      </div>
+    </div>
+
+    <!-- HEX 6 -->
+    <div class="w-40 md:w-48 relative">
+              <svg width="256" viewBox="0 0 31 29" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <clipPath id="hexClip1">
+            <path d="M9.00035 7.55536L2.02871 11.4285C1.39378 11.7812 1 12.4505 1 13.1768V21.8232C1 22.5495 1.39378 23.2188 2.02871 23.5715L9.00035 27.4446C9.61973 27.7887 10.3749 27.7794 10.9857 27.4202L17.514 23.58C18.1249 23.2206 18.5 22.5648 18.5 21.8561V13.1439C18.5 12.4352 18.1249 11.7794 17.514 11.42L10.9857 7.57981C10.375 7.22056 9.61973 7.21126 9.00035 7.55536Z"/>
+          </clipPath>
+        </defs>
+
+        <g transform="translate(6 -3)">
+          <image
+            href="{{ Vite::asset('resources/images/bee.webp') }}"
+            x="-4"
+            y="-2"
+            width="40"
+            height="40"
+            preserveAspectRatio="xMidYMid slice"
+            clip-path="url(#hexClip1)"
+          />
+        </g>
+      </svg>
+      <div class="absolute inset-x-0 bottom-0 text-xs uppercase text-center">
+        Daniel Semblano
+      </div>
+    </div>
+  </div>
+</div>
