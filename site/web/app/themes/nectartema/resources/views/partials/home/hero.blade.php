df82182d2399f136b5308d13da69832797717c25
Daniel Semblano <dsemblano@gmail.com>
2026-01-05 11:54:17 -0300

    New text for hero section

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -1,9 +1,8 @@
 <section id="hero" class="section-home text-white container h-fit">
     <div class="py-16 text-left prose  prose-a:no-underline">
         <h1 class="text-white text-4xl md:text-6xl">Mel Sustentável da Amazônia</h1>
-        <h2 class="text-white font-normal text-sm md:text-xl mb-10">Néctar da Amazônia: mel 100% puro de abelhas sem
-            ferrão,
-            artesanal, no Amapá e alinhado à bioeconomia e ao desenvolvimento sustentável.</h2>
+        <h2 class="text-white font-normal text-xl md:text-2xl mb-10">Néctar da Amazônia: O mais puro mel de abelhas sem
+            ferrão, direto do Amapá para o mundo</h2>
         <a href="/loja"
             class="px-4 py-4 rounded-lg text-xl font-semibold transition bg-melescuro hover:bg-primary text-white">Compre
             mel agora!</a>
