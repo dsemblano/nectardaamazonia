abb3208f6cc47d40a1bc448fe693e1e04057ea97
Daniel Semblano <dsemblano@gmail.com>
2026-01-14 17:58:14 -0300

    Hero section improvement

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -1,11 +1,13 @@
-<section id="hero" class="section-home text-white container">
-    <div class="py-16 text-left md:w-dvh">
-        <h1 class="text-white text-4xl md:text-6xl">Mel Sustentável da Amazônia</h1>
-        <h2 class="text-white font-normal text-xl md:text-2xl mb-10">Néctar da Amazônia: O mais puro mel de abelhas sem
-            ferrão, direto do Amapá para o mundo</h2>
-        <a href="/loja"
-            class="hero-cta">Compre
-            mel agora!</a>
+<section id="hero" class="section-home text-white flex flex-col justify-center">
+    <div class="container">
+        <div class="text-left md:w-dvh md:h-full">
+            <h1 class="text-white text-4xl md:text-6xl">Mel Sustentável da Amazônia</h1>
+            <h2 class="text-white font-normal text-xl md:text-2xl mb-10">Néctar da Amazônia: O mais puro mel de abelhas
+                sem
+                ferrão, direto do Amapá para o mundo</h2>
+            <a href="/loja" class="hero-cta">Compre
+                mel agora!</a>
+        </div>
     </div>
 </section>
 
