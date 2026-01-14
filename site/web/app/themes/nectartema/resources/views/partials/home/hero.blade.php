e4c779652b805f3b36e5970cf84e299ec33dd067
Daniel Semblano <dsemblano@gmail.com>
2026-01-14 18:29:27 -0300

    Hero section improvement fix on mobile

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -1,11 +1,11 @@
-<section id="hero" class="section-home text-white flex flex-col justify-center">
-    <div class="container">
-        <div class="text-left md:w-dvh md:h-full">
+<section id="hero" class="section-home text-white">
+    <div class="container h-dvh">
+        <div class="text-left h-dvh md:w-dvh md:h-full leading-loose flex flex-col justify-center">
             <h1 class="text-white text-4xl md:text-6xl">Mel Sustentável da Amazônia</h1>
             <h2 class="text-white font-normal text-xl md:text-2xl mb-10">Néctar da Amazônia: O mais puro mel de abelhas
                 sem
                 ferrão, direto do Amapá para o mundo</h2>
-            <a href="/loja" class="hero-cta">Compre
+            <a href="/loja" class="hero-cta w-fit">Compre
                 mel agora!</a>
         </div>
     </div>
