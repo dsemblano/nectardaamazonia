d2a818b855e08158e55c4c7af9e3ca6962251f61
Daniel Semblano <dsemblano@gmail.com>
2026-01-17 13:34:35 -0300

    Footer text changed

--- site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
@@ -3,7 +3,7 @@
         class="container max-w-none text-white prose:a:text-white">
         @php(dynamic_sidebar('sidebar-footer'))
         <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2 text-white">
-            <span class="z-10 font-bold text-white">© 2018 - {{ date('Y') }} 
+            <span class="z-10 font-bold text-white">© 2009 - {{ date('Y') }} 
                 <a class="text-white hover:underline" href="{{ home_url('/') }}">Néctar da Amazônia</a>
                 <span id="trademark" class="sup align-text-bottom">&reg;</span>
             </span>
