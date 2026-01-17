5839796004e8c5c626be2035ec41cba8d77a30af
Daniel Semblano <dsemblano@gmail.com>
2026-01-17 00:06:37 -0300

    Padding, margin and font sizes on home

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -2,7 +2,7 @@
     <div class="container h-dvh">
         <div class="text-left h-dvh md:w-dvh md:h-full leading-loose flex flex-col justify-center">
             <h1 class="text-white text-4xl md:text-6xl">Mel Sustentável da Amazônia</h1>
-            <p class="text-white font-normal text-2xl md:text-4xl mb-10">Néctar da Amazônia: O mais puro mel de abelhas
+            <p class="text-white font-normal text-2xl md:text-3xl mb-10">Néctar da Amazônia: O mais puro mel de abelhas
                 sem
                 ferrão, direto do Amapá para o mundo</p>
                 <a href="/loja" class="hero-cta w-fit">Compre
