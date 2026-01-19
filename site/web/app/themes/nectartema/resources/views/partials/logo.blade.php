c7589644b2c1e77986d4e68e5ca2f6541e218b48
Daniel Semblano <dsemblano@gmail.com>
2026-01-19 14:05:09 -0300

    Header and logo smaller

--- site/web/app/themes/nectartema/resources/views/partials/logo.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/logo.blade.php
@@ -1,5 +1,5 @@
-<a class="brand block max-w-[302px] w-full" href="{{ home_url('/') }}">
-  <div class="aspect-[302/84] w-full">
-    <img width="302" height="84" class="w-full h-full object-contain block hover:scale-105 transition duration-300 ease-in-out" id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/logonectarnovo.webp') }}" />
+<a class="brand block max-w-[200px] w-full md:mb-4" href="{{ home_url('/') }}">
+  <div class="aspect-[200/56] w-full">
+    <img width="200" height="56" class="w-full h-full object-contain block hover:scale-105 transition duration-300 ease-in-out" id="logo" alt="Logo da Néctar da Amazônia" src="{{ Vite::asset('resources/images/logonectarnovo.webp') }}" />
   </div>
 </a>
--- site/web/app/themes/nectartema/resources/views/sections/header.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/header.blade.php
@@ -1,5 +1,5 @@
-<header class="banner w-full z-50 top-0 left-0 bg-fundo shadow-md">
-    <div class="flex flex-row lg:flex-col lg:text-2xl text-p container py-4 gap-4">
+<header class="banner flex w-full z-50 top-0 left-0 bg-fundo shadow-md">
+    <div class="flex flex-row lg:flex-col text-p container justify-between">
         @include('partials.logo')
         @if (has_nav_menu('primary_navigation'))
             <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
