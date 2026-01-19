95e06671a352faebe0c25329722b63adcd359a24
Daniel Semblano <dsemblano@gmail.com>
2026-01-19 13:21:08 -0300

    Testing dynamic hero text desktop

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -2,10 +2,10 @@
     <div class="container h-dvh">
         <div class="text-left h-dvh md:w-dvh md:h-full leading-loose flex flex-col justify-center">
             <h1 class="text-white text-4xl md:text-6xl lg:text-7xl">Mel Sustentável da Amazônia</h1>
-            <p class="text-white font-normal text-2xl md:text-3xl">Néctar da Amazônia: <br> O mais puro mel de abelhas
+            <p class="text-white font-normal text-2xl md:text-4xl">Néctar da Amazônia: <br> O mais puro mel de abelhas
                 sem ferrão,
                 <span class="ticker-container">
-                    <span class="ticker-wrapper text-mel text-2xl md:text-3xl font-bold">
+                    <span class="ticker-wrapper text-mel font-bold">
                         <span class="item">direto do Amapá para o mundo</span>
                         <span class="item">fortalecendo a bioeconomia</span>
                         <span class="item">com propriedades medicinais</span>
