6ae7ec8c68096aa1861617d79a65a40ff4b35be5
Daniel Semblano <dsemblano@gmail.com>
2026-01-22 15:44:54 -0300

    min-height for product descriptions

--- site/web/app/themes/nectartema/resources/views/components/product-card.blade.php
+++ site/web/app/themes/nectartema/resources/views/components/product-card.blade.php
@@ -38,7 +38,7 @@
 
         {{-- SHORT DESCRIPTION --}}
         @if ($product->get_short_description())
-            <p class="text-sm text-gray-600">
+            <p class="text-sm text-gray-600 min-h-28">
                 {!! wp_strip_all_tags($product->get_short_description()) !!}
             </p>
         @endif
