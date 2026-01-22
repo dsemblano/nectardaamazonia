f94fd86a4ab3db6a3c818b1bdcd3955af4facb2f
Daniel Semblano <dsemblano@gmail.com>
2026-01-22 20:46:21 -0300

    Fix for menu mobile X appearing on loading

--- site/web/app/themes/nectartema/resources/views/partials/menu.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/menu.blade.php
@@ -20,7 +20,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"
                 color="var(--color-primary)" />
         </svg>
-        <svg x-show="mobileOpen" class="absolute inset-0 w-full h-full" fill="none" viewBox="0 0 24 24"
+        <svg x-show="mobileOpen" style="display: none" class="absolute inset-0 w-full h-full" fill="none" viewBox="0 0 24 24"
             stroke="var(--color-primary)">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
