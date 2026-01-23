91d8414b97f844235690661339a9c6e3db6cb806
Daniel Semblano <dsemblano@gmail.com>
2026-01-23 09:28:35 -0300

    Min-height for h3 Servicos section for tablet and desktop screen

--- site/web/app/themes/nectartema/resources/views/partials/home/servicos.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/servicos.blade.php
@@ -5,7 +5,7 @@
             <img width="188" height="215"
                 src="{{ Vite::asset('resources/images/servicos/Icone-Consultoria-tecnica.webp') }}"
                 alt="Ícone Consultoria para o Desenvolvimento da produção de mel" class="rounded-xl">
-            <h3>Consultoria para o Desenvolvimento da produção de mel</h3>
+            <h3 class="md:min-h-28">Consultoria para o Desenvolvimento da produção de mel</h3>
             <p>Fazemos diagnósticos detalhados, elaboramos projetos e gerimos meliponários, garantindo o sucesso e a
                 sustentabilidade na criação de abelhas e produção de mel.</p>
         </div>
@@ -13,7 +13,7 @@
             <img width="188" height="215"
                 src="{{ Vite::asset('resources/images/servicos/icone-estudos-socioambientais.webp') }}"
                 alt="Ícone Diagnósticos Socioambientais com Comunidades" class="rounded-xl">
-            <h3>Diagnósticos Socioambientais
+            <h3 class="md:min-h-28">Diagnósticos Socioambientais
                 com Comunidades</h3>
             <p>Apoiamos comunidades locais, incluindo quilombolas, na implementação de práticas de meliponicultura
                 como
@@ -23,7 +23,7 @@
             <img width="188" height="215"
                 src="{{ Vite::asset('resources/images/servicos/Icone-estudos-ambientais.webp') }}"
                 alt="Ícone Implantação de Biofábricas de Abelhas" class="rounded-xl">
-            <h3>Implantação de
+            <h3 class="md:min-h-28">Implantação de
                 Biofábricas de Abelhas</h3>
             <p>Desenvolvemos soluções inovadoras para enfrentar o déficit global de polinizadores, com foco na
                 preservação e expansão das abelhas nativas.</p>
@@ -32,7 +32,7 @@
             <img width="188" height="215" src="{{ Vite::asset('resources/images/servicos/icone-eventos.webp') }}"
                 alt="Ícone Eventos Sustentabilidade com Abelhas
 " class="rounded-xl">
-            <h3>Eventos
+            <h3 class="md:min-h-28">Eventos
                 Sustentabilidade
                 com Abelhas </h3>
             <p>Facilitamos palestras, workshops e eventos educativos que destacam a importância das abelhas para o
