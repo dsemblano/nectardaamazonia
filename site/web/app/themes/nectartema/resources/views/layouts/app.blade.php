4fcf56048e61bd7521640b73bb06335e87c82d4a
Daniel Semblano <dsemblano@gmail.com>
2026-06-28 23:56:38 -0300

    Adsense code on head

--- site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
+++ site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-adsense-account" content="ca-pub-5180633762059604">
     @php(do_action('get_header'))
     @php(wp_head())
 
