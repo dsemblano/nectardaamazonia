258d271ea07e3f758fb83401ef41fb983db908e8
Daniel Semblano <dsemblano@gmail.com>
2026-06-29 13:22:07 -0300

    Adsense script code on head

--- site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
+++ site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-adsense-account" content="ca-pub-5180633762059604">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5180633762059604"
+     crossorigin="anonymous"></script>
     @php(do_action('get_header'))
     @php(wp_head())
 
