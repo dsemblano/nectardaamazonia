2fb8c511f15e2e3eacdac0664948cb92bad346f2
Daniel Semblano <dsemblano@gmail.com>
2026-07-06 12:00:50 -0300

    Optimzed images webp

--- site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
+++ site/web/app/themes/nectartema/resources/views/sections/footer.blade.php
@@ -1,10 +1,10 @@
-<footer id="footer" class="content-info bg-verde text-white py-6">
+<footer id="footer" class="content-info bg-verde text-black py-6">
     <div
-        class="container max-w-none text-white prose:a:text-white">
+        class="container max-w-none text-black prose:a:text-black">
         @php(dynamic_sidebar('sidebar-footer'))
-        <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2 text-white">
+        <div class="text-sm mt-4 flex flex-col items-center copyright border-gray-500 border-t border-solid pt-8 gap-2 text-black">
             <span class="z-10 font-bold text-white">© 2009 - {{ date('Y') }} 
-                <a class="text-white hover:underline" href="{{ home_url('/') }}">Néctar da Amazônia</a>
+                <a class="text-black hover:underline text-white" href="{{ home_url('/') }}">Néctar da Amazônia</a>
                 <span id="trademark" class="sup align-text-bottom">&reg;</span>
             </span>
         </div>
