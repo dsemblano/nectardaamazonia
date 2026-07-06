433eb99400165f9f2356271ff5931cc995d3a003
Daniel Semblano <dsemblano@gmail.com>
2026-07-06 20:25:37 -0300

    preload assets

--- site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
+++ site/web/app/themes/nectartema/resources/views/layouts/app.blade.php
@@ -16,14 +16,14 @@
         };
     </script> --}}
 
-    {{-- @if (is_front_page() || is_home())
+    @if (is_front_page() || is_home())
         <link rel="preload" fetchpriority="high" as="image"
             href="{{ Vite::asset('resources/images/meliponarios.webp') }}" type="image/webp">
     @endif
 
     <link rel="preload" href="{{ Vite::asset('resources/fonts/Poppins/Poppins-Regular.ttf') }}" as="font"
         type="font/ttf" crossorigin>
-    <link rel="author" type="text/plain" href="{{ Vite::asset('resources/fonts/humans.txt') }}" /> --}}
+    <link rel="author" type="text/plain" href="{{ Vite::asset('resources/fonts/humans.txt') }}" />
     {{-- @include('partials.gtag') --}}
     {{-- @include('partials.partytown')
     @include('partials.gtm') --}}
