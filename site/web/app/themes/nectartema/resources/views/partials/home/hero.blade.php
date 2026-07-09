846c7f4f7b717b53da740d126a5997af4b37ba66
Daniel Semblano <dsemblano@gmail.com>
2026-07-09 13:11:58 -0300

    Hero images webp and aviff

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -1,16 +1,32 @@
 <section id="hero" class="section-home text-white relative overflow-hidden">
-    
-    <img id="hero-img"
-        src="{{ Vite::asset('resources/images/meliponarios.webp') }}" 
+
+    {{-- <img id="hero-img"
+        src="{{ Vite::asset('resources/images/meliponarios.webp') }}"
         fetchpriority="high" 
         class="hero-bg-img no-lazy" 
         alt="Meliponários da Amazônia"
-    >
-    
+    > --}}
+
+    <img id="hero-img" 
+     src="{{ Vite::asset('resources/images/meliponarios.webp') }}"
+     srcset="{{ Vite::asset('resources/images/meliponarios-mobile.webp') }} 324w, {{ Vite::asset('resources/images/meliponarios.webp') }} 1920w"
+     sizes="(max-width: 576px) 100vw, 100vw"
+     fetchpriority="high" 
+     class="hero-bg-img no-lazy" 
+     alt="Logo Néctar da Amazônia">
+
+    <picture class="hero-bg-img no-lazy">
+        <source srcset="{{ Vite::asset('resources/images/meliponar.avif') }}" type="image/avif">
+        <source srcset="{{ Vite::asset('resources/images/meliponar.webp') }}" type="image/webp">
+        <img id="hero-img" src="{{ Vite::asset('resources/images/meliponar.webp') }}" fetchpriority="high"
+            alt="Meliponários da Amazônia">
+    </picture>
+
     <div class="hero-bg-overlay"></div>
 
     <div class="container h-dvh relative z-10">
-        <div class="prose lg:prose-lg prose-a:no-underline text-left h-dvh md:w-dvh md:h-full leading-loose flex flex-col justify-center">
+        <div
+            class="prose lg:prose-lg prose-a:no-underline text-left h-dvh md:w-dvh md:h-full leading-loose flex flex-col justify-center">
             <h1 class="text-white text-4xl md:text-6xl lg:text-7xl">Mel Sustentável da Amazônia</h1>
             <p class="text-white font-normal text-2xl md:text-3xl mb-10">
                 Néctar da Amazônia: O mais puro mel de abelhas sem ferrão,
@@ -18,8 +34,8 @@
                     <span class="ticker-wrapper text-mel">
                         <span class="item">rico em antioxidantes</span>
                         <span class="item">do Amapá para o mundo</span>
-                        <span class="item">fortalecendo a bioeconomia</span>                        
-                        <span class="item">rico em antioxidantes</span> 
+                        <span class="item">fortalecendo a bioeconomia</span>
+                        <span class="item">rico em antioxidantes</span>
                     </span>
                 </span>
             </p>
