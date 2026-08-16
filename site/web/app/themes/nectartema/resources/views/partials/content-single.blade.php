fe10ac4c8bb85d388134815fc24cf44c1304a2c1
Daniel Semblano <dsemblano@gmail.com>
2026-08-16 12:47:11 -0300

    New single pages layout

--- site/web/app/themes/nectartema/resources/views/partials/content-single.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/content-single.blade.php
@@ -1,29 +1,39 @@
-<article class="prose lg:prose-lg max-w-xs md:max-w-4xl mx-auto prose-a:no-underline prose-h3:text-xl prose-h2:mt-0 prose-picture:mt-0" @php(post_class('h-entry'))>
-  <header>
-    <h1 class="p-name text-secondary">
-      {!! $title !!}
-    </h1>
-
-    <p class="my-3 excerpt">
-      {{ wp_trim_words(get_the_excerpt(), 40, '...') }}
-    </p>
-
-    @include('partials.entry-meta')
-  </header>
-
-  <hr class="h-0.5 my-4 bg-gray-400 border-0">
-
-  <div class="e-content">
-    @php(the_content())
+<div class="breadcrumb bg-footer">
+  <div class="breadcrumb-inner h-28 max-w-xs md:max-w-4xl mx-auto px-7 pb-10 pt-4 text-xs md:text-base">
+    {!! do_shortcode('[tsf_breadcrumb sep="⬢"]') !!}
+    {{-- {!! do_shortcode('[tsf_breadcrumb sep="🐝"]') !!} --}}
   </div>
+</div>
+<div class="article-wrapper bg-white max-w-xs md:max-w-4xl mx-auto rounded-3xl p-7 relative bottom-8 md:bottom-14 shadow-lg">
+    <article class="mx-auto prose lg:prose-lg prose-a:no-underline prose-h3:text-xl prose-h2:mt-0 prose-picture:mt-0 pt-0 md:pt-8"
+        @php(post_class('h-entry'))>
+        <header>
+            <h1 class="p-name text-secondary">
+                {!! $title !!}
+            </h1>
 
-  @if ($pagination())
-    <footer>
-      <nav class="page-nav" aria-label="Page">
-        {!! $pagination !!}
-      </nav>
-    </footer>
-  @endif
+            <p class="my-3 excerpt">
+                {{ wp_trim_words(get_the_excerpt(), 40, '...') }}
+            </p>
 
-  @php(comments_template())
-</article>
+            @include('partials.entry-meta')
+        </header>
+
+        <hr class="h-0.5 my-4 bg-gray-400 border-0">
+
+        <div class="e-content">
+            @php(the_content())
+        </div>
+
+        @if ($pagination())
+            <footer>
+                <nav class="page-nav" aria-label="Page">
+                    {!! $pagination !!}
+                </nav>
+            </footer>
+        @endif
+
+        @php(comments_template())
+    </article>
+
+</div>
