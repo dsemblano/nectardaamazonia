711fb4dc850efa5520c91bef3a1cc7df63319db4
Daniel Semblano <dsemblano@gmail.com>
2026-09-07 18:51:48 -0300

    New hero section

--- site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/home/hero.blade.php
@@ -11,30 +11,23 @@
         <source srcset="{{ Vite::asset('resources/images/loja.webp') }}" type="image/webp">
 
         <!-- 3. Native fallback (This is where your layout classes MUST sit) -->
-        <img id="hero-img" src="{{ Vite::asset('resources/images/loja.webp') }}" data-opt-lazy-loaded="false"
+        <img src="{{ Vite::asset('resources/images/loja.webp') }}" data-opt-lazy-loaded="false"
             fetchpriority="high" class="hero-bg-img no-lazy w-full object-cover optml-skip"
-            alt="Logo Néctar da Amazônia">
+            alt="">
     </picture>
     <div class="hero-bg-overlay"></div>
 
     <div class="container h-dvh relative z-10">
         <div
             class="prose lg:prose-lg prose-a:no-underline text-left h-dvh md:w-dvh md:h-full leading-loose flex flex-col justify-center pb-16">
-            <h1 class="text-white text-3xl md:text-4xl lg:text-5xl">Mel autêntico de abelhas sem ferrão da Floresta Amazônica</h1>
-            <p class="text-white font-normal text-xl md:text-3xl mb-10">
-                <span class="text-mel font-bold">Néctar da Amazônia</span>: Mel puro de abelhas sem ferrão, colhido de abelhas nativas e de flores da Amazônia.
-                {{-- <span class="ticker-container block md:inline-block text-mel">
-                    <span class="ticker-wrapper text-mel">
-                        <span class="item">rico em antioxidantes</span>
-                        <span class="item">do Amapá para o mundo</span>
-                        <span class="item">colhido de forma sustentável</span>
-                        <span class="item">rico em antioxidantes</span>
-                    </span>
-                </span> --}}
+            <p class="hero_eyebrow text-mel font-bold not-prose mb-2">Néctar da Amazônia</p>
+            <h1 class="text-white text-3xl md:text-4xl lg:text-5xl">O sabor da floresta em cada gota.</h1>
+            <p class="hero_description text-white font-normal text-xl md:text-3xl mb-10">
+                Mel autêntico de abelhas sem ferrão da Amazônia para uma experiência única de sabor e natureza.
             </p>
             <div class="flex flex-col md:flex-row gap-4">
-                <a href="/loja" class="hero-cta w-fit">Explore nossos méis 🍯 </a>
-                <a href="/orcamento-instalacao-de-meliponarios" class="hero-colmeia w-fit">Ter meu Meliponário 🏡🐝</a>
+                <a href="/loja" class="hero-cta w-fit">Explore nossos méis 🍯</a>
+                <a href="/orcamento-instalacao-de-meliponarios" class="hero-colmeia w-fit">Quero meu Meliponário 🏡🐝</a>
             </div>
         </div>
     </div>
