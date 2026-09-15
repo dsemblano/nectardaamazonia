8c17b3ba37e43b34446fbe99d2a43eba177a55d9
Daniel Semblano <dsemblano@gmail.com>
2026-09-15 15:31:50 -0300

    Author page with dynamic textarea from WordPress admin

--- site/web/app/themes/nectartema/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/nectartema/resources/views/partials/page-header.blade.php
@@ -2,3 +2,16 @@
     class="text-4xl md:text-6xl prose-h1:mt-8 page-header">
     <h1 class="{{ !is_woocommerce() && !is_cart() && !is_checkout() ? ' container' : '' }}">{!! $title !!}</h1>
 </div>
+
+@if (is_author())
+    @php
+        $author = get_queried_object();
+    @endphp
+    <section class="author container max-w-none prose lg:prose-lg">
+        @if (!empty($author->description))
+            <div class="author__bio">
+                {!! wpautop(wp_kses_post($author->description)) !!}
+            </div>
+        @endif
+    </section>
+@endif
